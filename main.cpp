1c9d964b15dd626b2a104e9c3c735d512be42a37
k023g0116@g.neec.ac.jp <k023g0116@g.neec.ac.jp>
2024-09-29 06:23:43 +0900

    実装したがmainにInputを導入していない

--- main.cpp
+++ main.cpp
@@ -523,7 +523,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ウィンドウの生成
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,	  // 利用するクラス名
-		L"CG2",				  // タイトルバーの文字(何でも良い)
+		L"GE3",				  // タイトルバーの文字(何でも良い)
 		WS_OVERLAPPEDWINDOW,  // よく見るウィンドウスタイル
 		CW_USEDEFAULT,		  // 表示X座標(Windowsに任せる)
 		CW_USEDEFAULT,		  // 表示Y座標(WindowsOSに任せる)
