31ae1ec91b107bac3c012874e1918be4b0721845
k023g0116@g.neec.ac.jp <k023g0116@g.neec.ac.jp>
2025-05-02 23:35:32 +0900

    23Pまで終了

---
+++ project/Resources/shaders/CopyImage.VS.hlsl
@@ -0,0 +1,27 @@
+#include "CopyImage.hlsli"
+
+static const int kNumVertex = 3;
+
+static const float4 kPositions[kNumVertex] =
+{
+    { -1.0f, 1.0f, 0.0f, 1.0f }, // 左上
+    { 3.0f, 1.0f, 0.0f, 1.0f },  // 右上
+    { -1.0f, -3.0f, 0.0f, 1.0f } // 左下
+};
+
+static const float2 kTexCoords[kNumVertex] =
+{
+    { 0.0f, 0.0f }, // 左上
+    { 2.0f, 0.0f }, // 右上
+    { 0.0f, 2.0f }  // 左下
+};
+
+VertexShaderOutput main(int vertexID : SV_VertexID)
+{
+    VertexShaderOutput output;
+
+    output.position = kPositions[vertexID];
+    output.texcoord = kTexCoords[vertexID];
+    
+    return output;
+}
