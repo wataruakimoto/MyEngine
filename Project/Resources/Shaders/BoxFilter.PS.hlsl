c9ff224d5308bd2f7eb0a714b9999d759a8219f1
wataruakimoto <k023g0116@g.neec.ac.jp>
2025-05-15 14:32:18 +0900

    5x5のBoxFilterができた

--- Project/Resources/Shaders/BoxFilter.PS.hlsl
+++ Project/Resources/Shaders/BoxFilter.PS.hlsl
@@ -15,6 +15,22 @@ static const float kKernel3x3[3][3] = {
     { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f }
 };
 
+static const float2 kIndex5x5[5][5] = {
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
+    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
+    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } }
+};
+
+static const float kKernel5x5[5][5] = {
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+};
+
 struct PixelShaderOutput {
     float4 color : SV_TARGET0;
 };
@@ -31,15 +47,28 @@ PixelShaderOutput main(VertexShaderOutput input) {
     output.color.a = 1.0f;
     
     //3x3ループ
-    for (int x = 0; x < 3; ++x) {
-        for (int y = 0; y < 3; ++y) {
+    //for (int x = 0; x < 3; ++x) {
+    //    for (int y = 0; y < 3; ++y) {
+    //        
+    //        // 現在のtexcoordを算出
+    //        float2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+    //        
+    //        // 色に1/9掛けて足す
+    //        float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+    //        output.color.rgb += fetchColor * kKernel3x3[x][y];
+    //    }
+    //}
+    
+    // 5x5ループ
+    for (int x = 0; x < 5; ++x) {
+        for (int y = 0; y < 5; ++y) {
             
             // 現在のtexcoordを算出
-            float2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            float2 texcoord = input.texcoord + kIndex5x5[x][y] * uvStepSize;
             
-            // 色に1/9掛けて足す
+            // 色に1/25掛けて足す
             float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-            output.color.rgb += fetchColor * kKernel3x3[x][y];
+            output.color.rgb += fetchColor * kKernel5x5[x][y];
         }
     }
     
