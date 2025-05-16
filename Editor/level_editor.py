ad7c0f3e6cdee5919c055c8e985c69f266d7c19d
WataruAkimoto <k023G0116@g.neec.ac.jp>
2025-05-16 12:59:32 +0900

    01_06実装

--- Editor/level_editor.py
+++ Editor/level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -68,7 +69,11 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.separator()
         # トップバーの「エディターメニュー」に項目(オペレータ)を追加
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text = MYADDON_OT_create_ico_sphere.bl_label)
-         # 区切り線
+        # 区切り線
+        self.layout.separator()
+        # トップバーの「エディターメニュー」に項目(オペレータ)を追加
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname, text = MYADDON_OT_export_scene.bl_label)
+        # 区切り線
         self.layout.separator()
         # トップバーの「エディターメニュー」に項目(オペレータ)を追加
         self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
@@ -110,10 +115,49 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
 
         #オペレータの命令終了を通知
         return {'FINISHED'}
+    
+# オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self, context):
+
+        print("シーン情報をExportします")
+
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            #型は Vector, Quaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+            #回転を Quaternion から Euler (3軸での回転角) に変換
+            rot = rot.to_euler()
+            #ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            #トランスフォーム情報を表示
+            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+            #親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            # 区切り用の空行
+            print()
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
+
+        return {'FINISHED'}
+    
 
 # Blenderに登録するクラスリスト
 classes = (
+    TOPBAR_MT_my_menu,
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
-    TOPBAR_MT_my_menu,
+    MYADDON_OT_export_scene,
 )
