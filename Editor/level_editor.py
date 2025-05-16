1edb578f750a4fcba2e5be0032ff1e13f6c2f11d
WataruAkimoto <k023G0116@g.neec.ac.jp>
2025-05-16 12:55:58 +0900

    01_03実装

--- Editor/level_editor.py
+++ Editor/level_editor.py
@@ -1,11 +1,27 @@
 import bpy
 
-#関数定義
-def moveVertex():
-    #頂点を移動
-    bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
-    
-for i in range(10):
-    
-    #関数呼び出し
-    moveVertex()
+# ブレンダーに登録するアドオン情報
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "Wataru Akimoto",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": ""
+}
+
+# アドオン有効化コールバック
+def register():
+    print("レベルエディタが有効化されました。")
+
+# アドオン無効化コールバック
+def unregister():
+    print("レベルエディタが無効化されました。")
+
+# テスト実行用コード
+if __name__ == "__main__":
+    register()
