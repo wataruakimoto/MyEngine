85f343e4341c8e8ff22ef40b949ee021c1604aa4
WataruAkimoto <k023G0116@g.neec.ac.jp>
2025-05-16 12:58:26 +0900

    01_05実装

--- Editor/level_editor.py
+++ Editor/level_editor.py
@@ -63,8 +63,12 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
     def draw(self, context):
 
         # トップバーの「エディターメニュー」に項目(オペレータ)を追加
-        self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text = MYADDON_OT_stretch_vertex.bl_label)
         # 区切り線
+        self.layout.separator()
+        # トップバーの「エディターメニュー」に項目(オペレータ)を追加
+        self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text = MYADDON_OT_create_ico_sphere.bl_label)
+         # 区切り線
         self.layout.separator()
         # トップバーの「エディターメニュー」に項目(オペレータ)を追加
         self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
@@ -75,5 +79,41 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         # ID指定でサブメニューを追加
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
+# オペレータ 頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x +=1.0
+        print("頂点を伸ばしました。")
+
+        #オペレータの命令終了を通知
+        return {'FINISHED'}
+    
+# オペレータ ICO球生成
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_objects"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+
+        #オペレータの命令終了を通知
+        return {'FINISHED'}
+
 # Blenderに登録するクラスリスト
-classes = (TOPBAR_MT_my_menu,)
+classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
+    TOPBAR_MT_my_menu,
+)
