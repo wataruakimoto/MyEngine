3099888500e14715ad9d4f761689815738325e0c
WataruAkimoto <k023G0116@g.neec.ac.jp>
2025-08-01 11:04:57 +0900

    Revert "更新できていなかったため再度プッシュ"
    This reverts commit 858d28062872df38cc227bdcd0b34c6003cbc9ca.

--- Editor/level_editor/my_menu.py
+++ Editor/level_editor/my_menu.py
@@ -4,8 +4,7 @@ import bpy
 from .stretch_vertex import MYADDON_OT_stretch_vertex
 from .create_ico_sphere import MYADDON_OT_create_ico_sphere
 from .export_scene import MYADDON_OT_export_scene
-from .spawn import MYADDON_OT_generate_player_symbol
-from .spawn import MYADDON_OT_generate_enemy_symbol
+from .spawn import MYADDON_OT_load_symbol
 
 # トップバー拡張メニュー
 class TOPBAR_MT_my_menu(bpy.types.Menu):
@@ -32,13 +31,12 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         # 区切り線
         self.layout.separator()
         # トップバーの「エディターメニュー」に項目(オペレータ)を追加
-        self.layout.operator(MYADDON_OT_generate_player_symbol.bl_idname, text = MYADDON_OT_generate_player_symbol.bl_label)
-        # トップバーの「エディターメニュー」に項目(オペレータ)を追加
-        self.layout.operator(MYADDON_OT_generate_enemy_symbol.bl_idname, text = MYADDON_OT_generate_enemy_symbol.bl_label)
+        self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
+
         # 区切り線
         self.layout.separator()
         # トップバーの「エディターメニュー」に項目(オペレータ)を追加
-        self.layout.operator("wm.url_open_preset", text = "Manual", icon = 'HELP')
+        self.layout.operator(MYADDON_OT_load_symbol.bl_idname, text = MYADDON_OT_load_symbol.bl_label)
     
     # 既存のメニューにサブメニューを追加
     def submenu(self, context):
--- Editor/level_editor/spawn.py
+++ Editor/level_editor/spawn.py
@@ -2,23 +2,8 @@ import bpy
 import os
 import bpy.ops
 
-#データテーブル
-class SpawnNames():
-
-    #インデックス
-    PROTOTYPE = 0 #プロトタイプのオブジェクト名
-    INSTANCE = 1 #量産時のオブジェクト名
-    FILENAME = 2 #リソースファイル名
-
-    names = {}
-
-    #names["キー"] = (プロトタイプのオブジェクト名、 量産時のオブジェクト名、 リソースファイル名)
-    names["Player"] = ("PrototypePlayerSpawn", "PlayerSpawn", "Player/player.obj")
-    names["Enemy"] = ("PrototypeEnemySpawn", "EnemySpawn", "Enemy/enemy.obj")
-
 #オペレータ 出現ポイントのシンボルを読み込む
 class MYADDON_OT_load_symbol(bpy.types.Operator):
-
     bl_idname = "myaddon.myaddon_ot_load_symbol"
     bl_label = "SpawnPointSymbol 読み込み"
     bl_description = "出現ポイントのシンボルを読み込みます"
@@ -26,21 +11,27 @@ class MYADDON_OT_load_symbol(bpy.types.Operator):
     #リドゥ、アンドゥ可能オプション
     bl_options = {'REGISTER', 'UNDO'}
 
-    #1モデルの読み込み処理
-    def load_obj(self, type):
+    prototype_object_name = "PrototypePlayerSpawn"
+    object_name = "PlayerSpawn"
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
 
         print("出現ポイントのシンボルを読み込みます")
 
         #重複ロード禁止
-        spawn_object = bpy.data.objects.get(SpawnNames.names[type][SpawnNames.PROTOTYPE])
+        spawn_object = bpy.data.objects.get(MYADDON_OT_load_symbol.prototype_object_name)
         if spawn_object is not None:
             return {'CANCELLED'}
 
         #スクリプトが配置されているディレクトリの名前を取得する
         addon_directory = os.path.dirname(__file__)
+        
+        #ディレクトリからのモデルファイルの相対パスを記述
+        relative_path = "player/player.obj"
 
         #合成してモデルファイルのフルパスを得る
-        full_path = os.path.join(addon_directory, SpawnNames.names[type][SpawnNames.FILENAME])
+        full_path = os.path.join(addon_directory, relative_path)
 
         #オブジェクトをインポート
         bpy.ops.wm.obj_import('EXEC_DEFAULT', filepath = full_path,
@@ -54,93 +45,13 @@ class MYADDON_OT_load_symbol(bpy.types.Operator):
         object = bpy.context.active_object
 
         #オブジェクト名を変更
-        object.name = SpawnNames.names[type][SpawnNames.FILENAME]
+        object.name = MYADDON_OT_load_symbol.prototype_object_name
 
         #オブジェクトの種類を設定
-        object["type"] = SpawnNames.names[type][SpawnNames.INSTANCE]
+        object["type"] = MYADDON_OT_load_symbol.object_name
 
         #メモリ上にはおいておくがシーンから外す
         bpy.context.collection.objects.unlink(object)
 
         #オペレータの命令終了を通知
-        return {'FINISHED'}
-    
-    #メニューを実行したときに呼ばれるコールバック関数
-    def execute(self, context):
-
-        #Playerオブジェクト読み込み
-        self.load_obj("Player")
-        #Enemyオブジェクト読み込み
-        self.load_obj("Enemy")
-
-        return {'FINISHED'}
-    
-#オペレータ 出現ポイントのシンボルを作成・配置する
-class MYADDON_OT_generate_symbol(bpy.types.Operator):
-
-    bl_idname = "myaddon.myaddon_ot_generate_symbol"
-    bl_label = "SpawnPointSymbol 作成"
-    bl_description = "出現ポイントのシンボルを作成します"
-
-    #リドゥ、アンドゥ可能オプション
-    bl_options = {'REGISTER', 'UNDO'}
-
-    #プロパティ(引数として渡せる)
-    type: bpy.props.StringProperty(name = "Type", default = "Player")
-
-    #メニューを実行したときに呼ばれるコールバック関数
-    def execute(self, context):
-
-        #読み込み済みのコピー元オブジェクトをを検索
-        spawn_object = bpy.data.objects.get(SpawnNames.names[self.type][SpawnNames.PROTOTYPE])
-
-        #まだ読み込んでいない場合
-        if spawn_object is None:
-            #読み込み済みオペレータを実行する
-            bpy.ops.myaddon.myaddon_ot_load_symbol('EXEC_DEFAULT')
-            # 再検索。今度は見つかるはず
-            spawn_object = bpy.data.objects.get(SpawnNames.names[self.type][SpawnNames.PROTOTYPE])
-
-        print("出現ポイントのシンボルを作成します")
-
-        #Blenderでの選択を解除する
-        bpy.ops.object.select_all(action='DESELECT')
-
-        #複製元の非表示オブジェクトを複製する
-        object = spawn_object.copy()
-
-        #複製したオブジェクトを現在のシーンにリンク(出現させる)
-        bpy.context.collection.objects.link(object)
-
-        #オブジェクト名を変更
-        object.name = SpawnNames.names[self.type][SpawnNames.INSTANCE]
-
-        return {'FINISHED'}
-    
-#オペレータ 自キャラ専用
-class MYADDON_OT_generate_player_symbol(bpy.types.Operator):
-
-    bl_idname = "myaddon.myaddon_ot_generate_player_symbol"
-    bl_label = "PlayerSpawnSymbol 作成"
-    bl_description = "自キャラの出現ポイントのシンボルを作成します"
-
-    #メニューを実行したときに呼ばれるコールバック関数
-    def execute(self, context):
-
-        bpy.ops.myaddon.myaddon_ot_generate_symbol('EXEC_DEFAULT', type = "Player")
-
-        return {'FINISHED'}
-    
-#オペレータ 敵キャラ専用
-class MYADDON_OT_generate_enemy_symbol(bpy.types.Operator):
-
-    bl_idname = "myaddon.myaddon_ot_generate_enemy_symbol"
-    bl_label = "EnemySpawnSymbol 作成"
-    bl_description = "敵キャラの出現ポイントのシンボルを作成します"
-
-    #メニューを実行したときに呼ばれるコールバック関数
-    def execute(self, context):
-
-        bpy.ops.myaddon.myaddon_ot_generate_symbol('EXEC_DEFAULT', type = "Enemy")
-
         return {'FINISHED'}
