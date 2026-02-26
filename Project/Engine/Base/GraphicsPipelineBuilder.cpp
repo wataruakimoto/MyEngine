99497b987b639d9b80feb61461cf755a878fc90f
k023g0116@g.neec.ac.jp <k023g0116@g.neec.ac.jp>
2026-02-26 16:04:06 +0900

    パイプライン修正途中

--- Project/Engine/Base/GraphicsPipelineBuilder.cpp
+++ Project/Engine/Base/GraphicsPipelineBuilder.cpp
@@ -1,4 +1,7 @@
 #include "GraphicsPipelineBuilder.h"
+#include "DirectXUtility.h"
+
+#include <cassert>
 
 using namespace Engine;
 
@@ -6,6 +9,90 @@ GraphicsPipelineBuilder::GraphicsPipelineBuilder() {
 }
 
 GraphicsPipelineBuilder::~GraphicsPipelineBuilder() {
+}
+
+void GraphicsPipelineBuilder::AddInputElement(const char* semanticName, UINT semanticIndex, DXGI_FORMAT format) {
+
+	D3D12_INPUT_ELEMENT_DESC inputElementDesc{};
+
+	inputElementDesc.SemanticName = semanticName;
+	inputElementDesc.SemanticIndex = semanticIndex;
+	inputElementDesc.Format = format;
+	inputElementDesc.InputSlot = 0;
+	inputElementDesc.AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	inputElementDesc.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+
+	inputElementDescs_.push_back(inputElementDesc);
+}
+
+void GraphicsPipelineBuilder::Build() {
+
+	// PipelineStateの設定
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC graphicsPipelineStateDesc{};
+	
+	// RootSignatureの設定
+	ConfigureRootSignature();
+	graphicsPipelineStateDesc.pRootSignature = rootSignature_.Get();
+
+	// VertexShaderの設定
+	ConfigureVertexShader();
+	graphicsPipelineStateDesc.VS = { vertexShaderBlob_->GetBufferPointer(), vertexShaderBlob_->GetBufferSize() };
+
+	// PixelShaderの設定
+	ConfigurePixelShader();
+	graphicsPipelineStateDesc.PS = { pixelShaderBlob_->GetBufferPointer(), pixelShaderBlob_->GetBufferSize() };
+
+	// BlendStateの設定
+	ConfigureBlendState();
+	graphicsPipelineStateDesc.BlendState = blendDesc_;
+
+	// SamplerStateの設定
+	graphicsPipelineStateDesc.SampleDesc.Count = 1;
+	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+
+	// RasterizerStateの設定
+	ConfigureRasterizerState();
+	graphicsPipelineStateDesc.RasterizerState = rasterizerDesc_;
+
+	// DepthStencilStateの設定
+	ConfigureDepthStencilState();
+	graphicsPipelineStateDesc.DepthStencilState = depthStencilDesc_;
+
+	// InputLayoutの設定
+	ConfigureInputLayout();
+	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc_;
+
+	// PrimitiveTopologyTypeの設定
+	graphicsPipelineStateDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+
+	// RTVFormatsの設定
+	graphicsPipelineStateDesc.NumRenderTargets = 1;
+	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+
+	// GraphicsPipelineStateの生成
+}
+
+void GraphicsPipelineBuilder::ConfigureRootSignature() {
+}
+
+void GraphicsPipelineBuilder::ConfigureVertexShader() {
+
+	// シェーダーのフルパスを作成
+	std::wstring vertexShaderFullPath = shaderDirectoryPath_ + vertexShaderFileName_;
+
+	// シェーダーをコンパイル
+	vertexShaderBlob_ = DirectXUtility::GetInstance()->CompileShader(vertexShaderFullPath, L"vs_5_0");
+	assert(vertexShaderBlob_ != nullptr);
+}
+
+void GraphicsPipelineBuilder::ConfigurePixelShader() {
+
+	// シェーダーのフルパスを作成
+	std::wstring pixelShaderFullPath = shaderDirectoryPath_ + pixelShaderFileName_;
+
+	// シェーダーをコンパイル
+	pixelShaderBlob_ = DirectXUtility::GetInstance()->CompileShader(pixelShaderFullPath, L"ps_5_0");
+	assert(pixelShaderBlob_ != nullptr);
 }
 
 void GraphicsPipelineBuilder::ConfigureBlendState() {
@@ -19,7 +106,7 @@ void GraphicsPipelineBuilder::ConfigureBlendState() {
 
 	D3D12_RENDER_TARGET_BLEND_DESC& renderTargetBlendDesc = blendDesc_.RenderTarget[0];
 
-	// ブレンドモードに応じて設定
+	// モードごとの設定
 	switch (blendMode_) {
 
 	// なし
@@ -73,7 +160,7 @@ void GraphicsPipelineBuilder::ConfigureBlendState() {
 
 		renderTargetBlendDesc.BlendEnable = true;				 // ブレンドの有効化
 		renderTargetBlendDesc.SrcBlend = D3D12_BLEND_ZERO;		 // ソースの値は使わない
-		renderTargetBlendDesc.DestBlend = D3D12_BLEND_SRC_COLOR; //
+		renderTargetBlendDesc.DestBlend = D3D12_BLEND_SRC_COLOR; // デストの値にソースの値を乗算
 		renderTargetBlendDesc.BlendOp = D3D12_BLEND_OP_ADD;		 // 加算
 		renderTargetBlendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;	 // ソースのアルファ値はそのまま
 		renderTargetBlendDesc.DestBlendAlpha = D3D12_BLEND_ZERO; // デストのアルファ値は使わない
@@ -89,30 +176,79 @@ void GraphicsPipelineBuilder::ConfigureRasterizerState() {
 
 	/// ===== ラスタライザーステートの基本設定 ===== ///
 
-	rasterizerDesc_.FillMode = D3D12_FILL_MODE_SOLID; //
+	rasterizerDesc_.FillMode = D3D12_FILL_MODE_SOLID; // 三角形の中を塗りつぶす
 
-	// カリングモードに応じて設定
+	// モードごとの設定
 	switch (cullMode_) {
 	
 	// カリングなし
 	case CullMode::None:
 
-		rasterizerDesc_.CullMode = D3D12_CULL_MODE_NONE;
+		rasterizerDesc_.CullMode = D3D12_CULL_MODE_NONE; // カリングしない(裏面も表示させる)
 
 		break;
 
 	// 前面カリング
 	case CullMode::Front:
 
-		rasterizerDesc_.CullMode = D3D12_CULL_MODE_FRONT;
+		rasterizerDesc_.CullMode = D3D12_CULL_MODE_FRONT; // 表面(反時計回り)を表示しない
 
 		break;
 
 	// 背面カリング
 	case CullMode::Back:
 
-		rasterizerDesc_.CullMode = D3D12_CULL_MODE_BACK;
+		rasterizerDesc_.CullMode = D3D12_CULL_MODE_BACK; // 裏面(時計回り)を表示しない
 
 		break;
 	}
+
+	rasterizerDesc_.FrontCounterClockwise = false;								    // 頂点の並びが時計回りなら正面
+	rasterizerDesc_.DepthBias = D3D12_DEFAULT_DEPTH_BIAS;						    // 深度バイアス値
+	rasterizerDesc_.DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;			    // 深度バイアスクランプ値
+	rasterizerDesc_.SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;   // 傾斜に応じた深度バイアス値
+	rasterizerDesc_.DepthClipEnable = true;										    // 深度クリッピングを有効化
+	rasterizerDesc_.MultisampleEnable = false;									    // マルチサンプルを無効化
+	rasterizerDesc_.AntialiasedLineEnable = false;								    // アンチエイリアスを無効化
+	rasterizerDesc_.ForcedSampleCount = 0;										    // 強制サンプル数
+	rasterizerDesc_.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF; // 保守的ラスタライザモードを無効化
+}
+
+void GraphicsPipelineBuilder::ConfigureDepthStencilState() {
+
+	/// ===== 深度ステンシルステートの基本設定 ===== ///
+
+	// モードごとの設定
+	switch (depthMode_) {
+
+	// 無効化
+	case DepthMode::Disabled:
+
+		depthStencilDesc_.DepthEnable = false;						    // 深度テストを無効化
+		depthStencilDesc_.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO; // 深度値の書き込みを禁止
+
+		break;
+
+	// 読み込みのみ
+	case DepthMode::ReadOnly:
+
+		depthStencilDesc_.DepthEnable = true;							// 深度テストを有効化
+		depthStencilDesc_.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO; // 深度値の書き込みを禁止
+
+		break;
+
+	// 読み書き可能
+	case DepthMode::ReadWrite:
+
+		depthStencilDesc_.DepthEnable = true;						   // 深度テストを有効化
+		depthStencilDesc_.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL; // 深度値の書き込みを許可
+
+		break;
+	}
+
+	depthStencilDesc_.DepthFunc = D3D12_COMPARISON_FUNC_LESS; // 小さい値ほど手前に描画
+	depthStencilDesc_.StencilEnable = false;				  // ステンシルテストを無効化
+}
+
+void GraphicsPipelineBuilder::ConfigureInputLayout() {
 }
--- Project/Engine/Base/GraphicsPipelineBuilder.h
+++ Project/Engine/Base/GraphicsPipelineBuilder.h
@@ -1,8 +1,16 @@
 #pragma once
 
 #include <d3d12.h>
+#include <dxcapi.h>
+#include <string>
+#include <vector>
+#include <wrl.h>
 
 namespace Engine {
+
+	/// ===== 前方宣言 ===== ///
+
+	class DirectXUtility;
 
 	/// <summary>
 	/// グラフィックスパイプラインビルダー
@@ -67,10 +75,38 @@ namespace Engine {
 		/// </summary>
 		~GraphicsPipelineBuilder();
 
+		/// <summary>
+		/// インプットエレメントの追加
+		/// </summary>
+		/// <param name="semanticName">セマンティック名</param>
+		/// <param name="semanticIndex">セマンティック番号</param>
+		/// <param name="format">フォーマット</param>
+		void AddInputElement(const char* semanticName, UINT semanticIndex, DXGI_FORMAT format);
+
+		/// <summary>
+		/// パイプラインを生成
+		/// </summary>
+		void Build();
+
 	///-------------------------------------------/// 
 	/// クラス内関数
 	///-------------------------------------------///
 	private:
+
+		/// <summary>
+		/// ルートシグネチャの設定
+		/// </summary>
+		void ConfigureRootSignature();
+
+		/// <summary>
+		/// 頂点シェーダーの設定
+		/// </summary>
+		void ConfigureVertexShader();
+
+		/// <summary>
+		/// ピクセルシェーダーの設定
+		/// </summary>
+		void ConfigurePixelShader();
 
 		/// <summary>
 		/// ブレンドステートの設定
@@ -82,15 +118,53 @@ namespace Engine {
 		/// </summary>
 		void ConfigureRasterizerState();
 
+		/// <summary>
+		/// 深度ステンシルステートの設定
+		/// </summary>
+		void ConfigureDepthStencilState();
+
+		/// <summary>
+		/// インプットレイアウトの設定
+		/// </summary>
+		void ConfigureInputLayout();
+
 	///-------------------------------------------/// 
 	/// セッター
 	///-------------------------------------------///
 	public:
 
+		void SetVertexShaderFileName(const std::wstring& fileName) { vertexShaderFileName_ = fileName; }
+
+		void SetPixelShaderFileName(const std::wstring& fileName) { pixelShaderFileName_ = fileName; }
+
+		void SetBlendMode(BlendMode blendMode) { blendMode_ = blendMode; }
+
+		void SetCullMode(CullMode cullMode) { cullMode_ = cullMode; }
+
+		void SetDepthMode(DepthMode depthMode) { depthMode_ = depthMode; }
+
 	///-------------------------------------------/// 
 	/// メンバ変数
 	///-------------------------------------------///
 	private:
+
+		// ルートシグネチャ
+		Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_ = nullptr;
+
+		// シェーダーファイルのディレクトリパス
+		const std::wstring shaderDirectoryPath_ = L"Resources/Shaders/";
+
+		// 頂点シェーダーのファイル名
+		std::wstring vertexShaderFileName_ = L"";
+
+		// 頂点シェーダーのデータ
+		Microsoft::WRL::ComPtr<IDxcBlob> vertexShaderBlob_ = nullptr;
+
+		// ピクセルシェーダーのファイル名
+		std::wstring pixelShaderFileName_ = L"";
+
+		// ピクセルシェーダーのデータ
+		Microsoft::WRL::ComPtr<IDxcBlob> pixelShaderBlob_ = nullptr;
 
 		// ブレンドステートの設定
 		D3D12_BLEND_DESC blendDesc_ = {};
@@ -98,11 +172,29 @@ namespace Engine {
 		// ブレンドモード
 		BlendMode blendMode_ = BlendMode::None;
 
-		// ラスタライザーステート
+		// ラスタライザーステートの設定
 		D3D12_RASTERIZER_DESC rasterizerDesc_ = {};
 
 		// カリングモード
 		CullMode cullMode_ = CullMode::None;
+
+		// 深度ステンシルステートの設定
+		D3D12_DEPTH_STENCIL_DESC depthStencilDesc_ = {};
+
+		// 深度モード
+		DepthMode depthMode_ = DepthMode::Disabled;
+
+		// インプットエレメントの設定の動的配列
+		std::vector<D3D12_INPUT_ELEMENT_DESC> inputElementDescs_;
+
+		// インプットレイアウトの設定
+		D3D12_INPUT_LAYOUT_DESC inputLayoutDesc_ = {};
+
+		// グラフィックスパイプラインステート
+		Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_ = nullptr;
+
+		// DirectXUtilityのインスタンス
+		DirectXUtility* dxUtility_ = nullptr;
 	};
 }
 
